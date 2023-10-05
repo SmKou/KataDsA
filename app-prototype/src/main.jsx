a9162251e2ba5e584465959e67cd11498e848615
SmKou <66178856+SmKou@users.noreply.github.com>
2023-10-05 16:49:57 -0700

    Dev point

--- app-prototype/src/main.jsx
+++ app-prototype/src/main.jsx
@@ -6,9 +6,11 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <NextUIProvider>
-        <NextThemesProvider attribute="class" defaultTheme="dark">
-            <App />
-        </NextThemesProvider>
-    </NextUIProvider>
+    <React.StrictMode>
+        <NextUIProvider>
+            <NextThemesProvider attribute="class" defaultTheme="dark">
+                <App />
+            </NextThemesProvider>
+        </NextUIProvider>
+    </React.StrictMode>
 )
